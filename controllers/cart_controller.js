68ce75965d3e2ceea1046049ab1eca8eac7f26d9
MihirVir <mihirvir08@gmail.com>
2023-01-29 13:42:55 +0530

    changed dashboard and cart controller file

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -1,10 +1,19 @@
 const Cart = require('../models/cart');
-
+const Course = require('../models/course');
 const createNewCart = async (req, res) => {
     try {
         const userId = req.user.id;
         const courseId = req.params.id;
-        
+        const discountCoupon = req.body.discountCoupon;
+        const creatingCart = new Cart();
+
+        // findingCourse 
+        const courseAdded = await Course.findOne({
+            id: courseId
+        })
+
+        let initPrice = courseAdded.price;
+        //TODO Later
     } catch (err) {
         return res
                 .status(500)
@@ -14,6 +23,17 @@ const createNewCart = async (req, res) => {
     }
 }
 
+const clearCart = async (req, res) => {
+    try {
+
+    } catch (err) {
+        return res
+                .status(500)
+                .json({
+                    message: "Internal Server Error"
+                })
+    }
+}
 module.exports = {
     createNewCart
 }
--- controllers/dashboard_controller.js
+++ controllers/dashboard_controller.js
@@ -1,19 +1,83 @@
 const Purchased = require('../models/purchased');
 const Review = require('../models/review');
+const Course = require('../models/course');
 
 const getYearDataPurchase = async (req, res) => {
     try {
         const userId = req.user.id;
-
-        const coursesPurchasedByOthers = await Purchased.find();
+        console.log("user id = ", userId);
+        const findAllCourses = await Course.find({author: userId});
+                                                
         
-        if (userId !== coursesPurchasedByOthers) {
-            return res
+        if (!findAllCourses) {
+            return res  
                     .status(404)
                     .json({
-                        message: "Unauthorize access of the data"
+                        message: "Couldn't find any courses"
                     })
         }
+        let arr = []
+        findAllCourses.forEach(item => {
+            arr.push(item.id)
+        })
+        
+        const coursesPurchasedByOthers = await Purchased.find({coursesPurchased: {$in: arr}});
+        let yearCount = 0;
+        let monthCount = 0;
+        let janCount = 0;
+        let febCount = 0;
+        let marchCount = 0;
+        let aprilCount = 0;
+        let mayCount = 0;
+        let juneCount = 0;
+        let julyCount = 0;
+        coursesPurchasedByOthers.forEach(course => {
+            
+            const current = new Date();
+            const currentYear = current.getYear();
+            const currentMonth = current.getMonth();
+            
+            console.log(currentYear);
+            // getting course details
+            const purchaseDetails = course.createdAt.getYear();
+            const purchaseMonth = course.createdAt.getMonth();
+            console.log(purchaseDetails);
+            
+            if (currentYear - purchaseDetails === 0) {
+                // counting 
+                yearCount++;
+            }
+            if (currentMonth - purchaseMonth <= 6 ) {
+                monthCount++;
+            }
+            // setting monthly count for details ig
+            console.log(course.createdAt.getMonth());
+            if ((currentYear - purchaseDetails === 0) && (course.createdAt.getMonth() === 0)) {
+                janCount++;
+            }
+            if ((currentYear - purchaseDetails === 0) && (course.createdAt.getMonth() === 1)) {
+                febCount++;
+            }
+            if ((currentYear - purchaseDetails === 0) && (course.createdAt.getMonth() === 2)) {
+                marchCount++;
+            }
+            if ((currentYear - purchaseDetails === 0) && (course.createdAt.getMonth() === 3)) {
+                aprilCount++;
+            }
+            if ((currentYear - purchaseDetails === 0) && (course.createdAt.getMonth() === 4)) {
+                mayCount++;
+            }
+            if ((currentYear - purchaseDetails === 0) && (course.createdAt.getMonth() === 5)) {
+                juneCount++;
+            }
+            if ((currentYear - purchaseDetails === 0) && (course.createdAt.getMonth() === 6)) {
+                julyCount++;
+            }
+            return res
+                    .status(200)
+                    .json({yearCount, monthCount, janCount, febCount, marchCount, aprilCount, mayCount, juneCount, julyCount})
+        })
+        
         
     } catch (err) {
         console.log(err);
