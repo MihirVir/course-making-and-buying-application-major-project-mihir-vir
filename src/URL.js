f040fcf053964b1d18a141d3c402350cf36e01a3
MihirVir <80036612+MihirVir@users.noreply.github.com>
2023-05-01 19:42:46 +0530

    changed url and meta data

--- src/URL.js
+++ src/URL.js
@@ -1,2 +1,2 @@
-// export const URL = `https://backend-course-app-production-1670.up.railway.app/`;
-export const URL = `http://localhost:8000/`;
+export const URL = `https://backend-course-app-production-1670.up.railway.app/`;
+// export const URL = `http://localhost:8000/`;
